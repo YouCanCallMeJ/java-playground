280349f1c2c65041a3f3a68f38abfc43538411ad
You can call me J <jsy724724@gmail.com>
2021-07-18 22:16:32 +0900

    Update Project Calendar
    2021-07-18
    1. Calculate Days in a Month

--- Calendar/src/calendar/Sum.java
+++ Calendar/src/calendar/Sum.java
@@ -2,23 +2,26 @@ package calendar;
 
 import java.util.Scanner;
 
-public class Calendar {
+public class Sum {
 
 	public static void main(String[] args) {
-		System.out.print("달과 연을 입력하세요: ");
+		System.out.print("두수를 입력하세요: ");
 
 		Scanner scanner = new Scanner(System.in);
 		String[] numbersStrings = scanner.nextLine().split(" ");
-		
+
 		int[] ints = new int[numbersStrings.length];
 
 		for (int i = 0; i < ints.length; i++) {
 			ints[i] = Integer.parseInt(numbersStrings[i]);
 		}
-		
-		DIM dim = new DIM(ints[1], ints[0]);
-		
-		System.out.println(ints[0] + "년 " + ints[1] + "월은 " + dim.getD() + "입니다.");
+
+		int sum = 0;
+		for (int i = 0; i < ints.length; i++) {
+			sum += ints[i];
+		}
+
+		System.out.println("두수의 합은 " + sum + "입니다.");
 	}
 
 }
