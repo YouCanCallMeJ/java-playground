c6adab3d88cdf8de3919fc0c79b34931bb31375c
You can call me J <jsy724724@gmail.com>
2021-07-18 18:45:17 +0900

    Update GuGuDan
    2021-07-18
    version 4: Class 활용 OOP

--- GuGuDan/src/gugudan/GuGuDan.java
+++ GuGuDan/src/gugudan/GuGuDan.java
@@ -9,33 +9,16 @@ public class GuGuDan {
 		
 		boolean iterate = true;
 		
-		// version 3: 메소드 활용
+		// version 4: 클래스 OOP 활용
 		while (iterate) {
 			System.out.print("출력을 원하는 단을 입력해주세요:");
 			Scanner scanner = new Scanner(System.in);
 			int dan = scanner.nextInt();
-			int[] results = calculate(dan);
-
-			print(dan, results);
+			
+			Calculation cal = new Calculation(dan);
+			cal.print();
 			
 			System.out.println("======================");
-		}
-	}
-	
-	public static int[] calculate(int dan) {
-		
-		int[] results = new int[9];
-		
-		for (int i = 0; i < 9; i++) {
-			results[i] = dan * (i + 1);
-		}
-		
-		return results;
-	}
-
-	public static void print(int dan, int[] results) {
-		for (int i = 0; i < 9; i++) {
-			System.out.println(dan + " * " + (i + 1) + " = " + results[i]);
 		}
 	}
 }
