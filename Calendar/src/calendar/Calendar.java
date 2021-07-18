4a8e93b14f26bf94ba158e598e03ca5db80a7412
You can call me J <jsy724724@gmail.com>
2021-07-18 23:01:13 +0900

    Update Project Calendar
    2021-07-18
    1. Endless Input
    2. End Condition (Enter -1 )

--- Calendar/src/calendar/Calendar.java
+++ Calendar/src/calendar/Calendar.java
@@ -6,35 +6,28 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("반복횟수를 입력하세요: ");
-		int times = Integer.parseInt(scanner.nextLine());
-		
-		int[][] months = new int[times][2];
-		for (int i = 0; i < times; i++) {
-			System.out.println("달과 연을 입력하세요: ");
+
+		while (true) {
+			int[][] months = new int[1][2];
+
+			System.out.println("달과 연을 입력하세요. 종료하시려면 -1을 입력하세요: ");
 			String[] numbersStrings = scanner.nextLine().split(" ");
+
+			if (Integer.parseInt(numbersStrings[0]) == -1) {
+				break;
+			}
 			
 			int[] ints = new int[numbersStrings.length];
 
-			for (int i1 = 0; i1 < ints.length; i1++) {
-				ints[i1] = Integer.parseInt(numbersStrings[i1]);
+			for (int i = 0; i < ints.length; i++) {
+				ints[i] = Integer.parseInt(numbersStrings[i]);
 			}
-			
-			months[i][0] = ints[0];
-			months[i][1] = ints[1];
+
+			DIM dim = new DIM(ints[0], ints[1]);
+
+			System.out.println(ints[1] + "년 " + ints[0] + "월은 " + dim.getD() + "입니다.");
 		}
-		
-		DIM[] dims = new DIM[times];
-		for (int i = 0; i < dims.length; i++) {
-			dims[i] = new DIM(months[i][0], months[i][1]);
-		}
-		
-		for (int i = 0; i < dims.length; i++) {
-			System.out.println(months[i][1] + "년 " + months[i][0] + "월은 " + dims[i].getD() + "입니다.");
-		}
-		
-		
+
 	}
 
 }
