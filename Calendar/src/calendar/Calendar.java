686ce613ef8c2f36af8cf3f5a28da1d0d10464fd
You can call me J <jsy724724@gmail.com>
2021-07-18 21:21:02 +0900

    Update Project Calendar
    2021-07-18
    1. Add Function Sum

--- Calendar/src/calendar/Calendar.java
+++ Calendar/src/calendar/Calendar.java
@@ -5,7 +5,24 @@ import java.util.Scanner;
 public class Calendar {
 
 	public static void main(String[] args) {
-
+		
+		System.out.print("두수를 입력하세요: ");
+		
+		Scanner scanner = new Scanner(System.in);
+		String[] numbersStrings = scanner.nextLine().split(" ");
+		
+		int[] ints = new int[numbersStrings.length];
+		
+		for (int i = 0; i < ints.length; i++) {
+			ints[i] = Integer.parseInt(numbersStrings[i]); 
+		}
+		
+		int sum = 0;
+		for (int i = 0; i < ints.length; i++) {
+			sum += ints[i]; 
+		}
+		
+		System.out.println("두수의 합은 " + sum + "입니다.");
 	}
 
 }
