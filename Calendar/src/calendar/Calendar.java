e8ac0dffe19e667782e7a4fdbadcd75bcc6455b0
You can call me J <jsy724724@gmail.com>
2021-07-18 22:52:15 +0900

    Update Project Calendar
    2021-07-18
    Requirements
    1. Enter Iterate Times

--- Calendar/src/calendar/Calendar.java
+++ Calendar/src/calendar/Calendar.java
@@ -5,20 +5,36 @@ import java.util.Scanner;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.print("달과 연을 입력하세요: ");
-
 		Scanner scanner = new Scanner(System.in);
-		String[] numbersStrings = scanner.nextLine().split(" ");
 		
-		int[] ints = new int[numbersStrings.length];
+		System.out.print("반복횟수를 입력하세요: ");
+		int times = Integer.parseInt(scanner.nextLine());
+		
+		int[][] months = new int[times][2];
+		for (int i = 0; i < times; i++) {
+			System.out.println("달과 연을 입력하세요: ");
+			String[] numbersStrings = scanner.nextLine().split(" ");
+			
+			int[] ints = new int[numbersStrings.length];
 
-		for (int i = 0; i < ints.length; i++) {
-			ints[i] = Integer.parseInt(numbersStrings[i]);
+			for (int i1 = 0; i1 < ints.length; i1++) {
+				ints[i1] = Integer.parseInt(numbersStrings[i1]);
+			}
+			
+			months[i][0] = ints[0];
+			months[i][1] = ints[1];
 		}
 		
-		DIM dim = new DIM(ints[1], ints[0]);
+		DIM[] dims = new DIM[times];
+		for (int i = 0; i < dims.length; i++) {
+			dims[i] = new DIM(months[i][0], months[i][1]);
+		}
 		
-		System.out.println(ints[0] + "년 " + ints[1] + "월은 " + dim.getD() + "입니다.");
+		for (int i = 0; i < dims.length; i++) {
+			System.out.println(months[i][1] + "년 " + months[i][0] + "월은 " + dims[i].getD() + "입니다.");
+		}
+		
+		
 	}
 
 }
