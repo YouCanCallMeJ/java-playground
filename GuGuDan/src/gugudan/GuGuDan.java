904c3a21dc6fdfaa0cc8c16407a09185e439535b
You can call me J <jsy724724@gmail.com>
2021-07-18 15:53:53 +0900

    Update GuGuDan.java
    2021-07-18
    version 3: 메소드 활용

--- GuGuDan/src/gugudan/GuGuDan.java
+++ GuGuDan/src/gugudan/GuGuDan.java
@@ -9,23 +9,33 @@ public class GuGuDan {
 		
 		boolean iterate = true;
 		
-		// version 2: 배열 활용
+		// version 3: 메소드 활용
 		while (iterate) {
 			System.out.print("출력을 원하는 단을 입력해주세요:");
 			Scanner scanner = new Scanner(System.in);
 			int dan = scanner.nextInt();
-			int[] results = new int[9];
-			
-			for (int i = 0; i < 9; i++) {
-				results[i] = dan * (i + 1);
-			}
-			
-			for (int i = 0; i < 9; i++) {
-				System.out.println(dan + " * " + (i + 1) + " = " + results[i]);
-			}
+			int[] results = calculate(dan);
+
+			print(dan, results);
 			
 			System.out.println("======================");
 		}
 	}
+	
+	public static int[] calculate(int dan) {
+		
+		int[] results = new int[9];
+		
+		for (int i = 0; i < 9; i++) {
+			results[i] = dan * (i + 1);
+		}
+		
+		return results;
+	}
 
+	public static void print(int dan, int[] results) {
+		for (int i = 0; i < 9; i++) {
+			System.out.println(dan + " * " + (i + 1) + " = " + results[i]);
+		}
+	}
 }
