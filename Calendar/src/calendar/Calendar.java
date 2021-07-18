671689f798791a7d89d72db22d6ff488911655d9
You can call me J <jsy724724@gmail.com>
2021-07-18 23:20:12 +0900

    Update Project Calendar
    2021-07-18
    1. Print Formatted Calendar

--- Calendar/src/calendar/Calendar.java
+++ Calendar/src/calendar/Calendar.java
@@ -1,5 +1,6 @@
 package calendar;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calendar {
@@ -7,9 +8,8 @@ public class Calendar {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		while (true) {
+		while (true) {			
 			int[][] months = new int[1][2];
-
 			System.out.println("달과 연을 입력하세요. 종료하시려면 -1을 입력하세요: ");
 			String[] numbersStrings = scanner.nextLine().split(" ");
 
@@ -23,9 +23,27 @@ public class Calendar {
 				ints[i] = Integer.parseInt(numbersStrings[i]);
 			}
 
+			// Print Calendar Head
 			DIM dim = new DIM(ints[0], ints[1]);
-
 			System.out.println(ints[1] + "년 " + ints[0] + "월은 " + dim.getD() + "입니다.");
+			System.out.println("일\t월\t화\t수\t목\t금\t토");
+			System.out.println("--------------------------------------------------");
+			
+			// Assign days to array 
+			int[] days = new int[dim.getD()];
+			for (int i = 0; i < days.length; i++) {
+				days[i] = (i + 1);
+			}
+
+			for (int i = 0; i < days.length; i++) {
+				if (i != 0 && i % 7 == 0) {
+					System.out.println();
+				}
+				System.out.print(days[i] + "\t");
+				
+			}
+			
+			System.out.println();
 		}
 
 	}
