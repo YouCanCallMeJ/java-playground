5154dc3ea7db5870b331ee6b634d9a439614707f
You can call me J <jsy724724@gmail.com>
2021-07-21 01:11:43 +0900

    Update Project Scheduler
    2021-07-21
    1. Create Member Variable
    2. Using OOP
    3. Organize Calendar class

--- Scheduler/src/calendar/Calendar.java
+++ Scheduler/src/calendar/Calendar.java
@@ -1,39 +1,96 @@
 package calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 	
-	private int[] days;
+	private String yearString;
+	private String monthString;
+	private String dayString;
+	private String startDayString;
+	private DIM dim;
+	private String[][] days;
+	private int offset;
 	
-	public int[] getDays() {
+	public String[][] getDays() {
 		return days;
 	}
 
-	public void saveSchedule(String yearString, String monthString, String dayString, String startDayString) {
+	public void setDays(String[][] days) {
+		this.days = days;
+	}
+
+	public DIM getDim() {
+		return dim;
+	}
+
+	public void setDim(DIM dim) {
+		this.dim = dim;
+	}
+
+	public String getYearString() {
+		return yearString;
+	}
+
+	public void setYearString(String yearString) {
+		this.yearString = yearString;
+	}
+
+	public String getMonthString() {
+		return monthString;
+	}
+
+	public void setMonthString(String monthString) {
+		this.monthString = monthString;
+	}
+
+	public String getStartDayString() {
+		return startDayString;
+	}
+
+	public String getDayString() {
+		return dayString;
+	}
+
+	public void setDayString(String dayString) {
+		this.dayString = dayString;
+	}
+	
+	public void setStartDayString(String startDayString) {
+		this.startDayString = startDayString;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+	
+	public Calendar(String yearString, String monthString, String dayString, String startDayString) {
+		this.yearString = yearString;
+		this.monthString = monthString;
+		this.dayString = dayString;
+		this.startDayString = startDayString;
+		this.offset = calculateOffset(startDayString);
 
 		// Calculate Days in a month
-		DIM dim = new DIM(Integer.parseInt(monthString), Integer.parseInt(yearString));
-
-		// Print Calendar Head
-		System.out.println(yearString + "년 " + monthString + "월");
-		System.out.println("일\t월\t화\t수\t목\t금\t토");
-		System.out.println("--------------------------------------------------");
-		
-		// Calculate Offset
-		int offset = calculateOffset(startDayString);
+		this.dim = new DIM(Integer.parseInt(monthString), Integer.parseInt(yearString));
 		
 		// Assign days to array
-		int[] days = new int[dim.getD() + offset];
-		for (int i = 0; i < days.length; i++) {
+		this.days = new String[2][dim.getD() + offset];
+		for (int i = 0; i < dim.getD() + offset; i++) {
 			if(i < offset) {
-				days[i] = 0;
+				days[0][i] = "0";
 			} else {
-				days[i] = (i + 1 - offset);
+				days[0][i] = Integer.toString((i + 1 - offset));
 			}
 		}
-		
-		System.out.println();
+	}
+	
+	
+	public void saveSchedule(String toDoString) {
+		int day = Integer.parseInt(dayString);
+		days[1][day - 1 + offset] = toDoString;
 	}
 	
 	// Calculate Offset
@@ -60,15 +117,42 @@ public class Calendar {
 	}
 	
 	public void printCalender() {
-		for (int i = 0; i < days.length; i++) {
+		// Print Calendar Head
+		System.out.println(this.yearString + "년 " + this.monthString + "월");
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		System.out.println("--------------------------------------------------");
+		
+		for (int i = 0; i < days[0].length; i++) {
 			if (i != 0 && i % 7 == 0) {
 				System.out.println();
+				for (int j = i - 7; j <= i; j++) {
+					if (days[1][j] != null) {
+						System.out.print(".\t");
+					} else {
+						System.out.print(" \t");
+					}
+				}
+				System.out.println();
 			}
-			if(days[i] == 0) {
+			if(days[0][i] == "0") {
 				System.out.print(" \t");
 			} else {
-				System.out.print(days[i] + "\t");
-			}			
+				System.out.print(days[0][i] + "\t");
+			}
+			
+			if (i == days[0].length - 1) {
+				System.out.println();
+				for (int j = i - 7; j <= i; j++) {
+					if (days[1][j] != null) {
+						System.out.print(".\t");
+					} else {
+						System.out.print(" \t");
+					}
+				}
+				System.out.println();
+			}
 		}
+		
+		System.out.println();
 	}
 }
--- Scheduler/src/scehduler/SchedulerMain.java
+++ Scheduler/src/scehduler/SchedulerMain.java
@@ -11,16 +11,18 @@ import calendar.Calendar;
 public class SchedulerMain {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
 		System.out.println("+----------------------+\r\n"
 				+ "| 1. 일정 등록\r\n"
 				+ "| 2. 일정 검색\r\n"
 				+ "| 3. 달력 보기\r\n"
 				+ "| h. 도움말 q. 종료\r\n"
 				+ "+----------------------+");
-		while (true) {
+		
+		while (true) {			
+			// 명렁어 입력
 			System.out.println("명령 (1, 2, 3, h, q)");
-			
-			Scanner scanner = new Scanner(System.in);
 			String command = scanner.nextLine();
 			
 			switch (command) {
@@ -35,8 +37,15 @@ public class SchedulerMain {
 					System.out.println("첫번째 요일을 입력하세요. (SU, MO, WE, TH, FR, SA)");
 					String startDayString = scanner.nextLine();
 
-					Calendar calendar = new Calendar();
-					calendar.saveSchedule(yearString, monthString, dayString, startDayString);
+					Calendar calendar = new Calendar(yearString, monthString, dayString, startDayString);
+					
+					// 
+					System.out.println("일정을 입력하세요.");
+					calendar.saveSchedule(scanner.nextLine());
+					System.out.println("일정이 등록되었습니다.");
+					calendar.printCalender();
+				}
+				case "3": {
 					
 				}
 				default:
